09509a610af36b386d8750e62b0a92706387df62
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-05 17:40:19 -0400

    fixes environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,11 @@
 export const environment = {
   production: false,
   //local
-  baseUrl: 'http://localhost:3051/api',
-  mediaUrl: 'http://localhost:3051/api/uploads',
-  // mediaUrl: 'http://localhost:3010/api/uploads-cloudinary',
+  baseUrl: 'http://localhost:3005/api',
+  mediaUrl: 'http://localhost:3005/api/uploads',
+
   cloudinaryMediaURl: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741194325/',
-  soketServer : 'http://localhost:3051',
-  
-  
-  //remoto vercel
-  // baseUrl: 'https://back-ecomm-mall.vercel.app/api',
-  // mediaUrl: 'https://back-ecomm-mall.vercel.app/api/uploads/',
-  // soketServer : 'https://back-ecomm-mall.vercel.app/',
+  soketServer : 'http://localhost:3005',
   //plugins
   rapidapiKey: 'a7036a3222mshc2920e679cd1cafp141e56jsn81cbe707ac15',
   rapidapiHost: 'apidojo-17track-v1.p.rapidapi.com',
@@ -23,12 +17,3 @@ export const environment = {
   clientIdPaypal: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
   sandboxPaypal: 'AQhKPBY5mgg0JustLJCcf6ncmd9RghCiNhXT_b6rNUakyQtnEn8MzCn_dkHAyt5n7_P0Omo5M05to5j0'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
