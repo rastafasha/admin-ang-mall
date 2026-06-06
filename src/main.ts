154795c0cb56824c1ef6b91b2db4f85119e805f5
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-06-05 21:16:55 -0400

    correchion app module y maints

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import '@angular/compiler';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-if (environment.production) {enableProdMode();}
-   platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    // Si el navegador soporta Service Workers y estamos en producción, lo registramos a la fuerza
+    if ('serviceWorker' in navigator && environment.production) {
+      // CORRECCIÓN: Quitamos el '/' inicial para que la ruta sea relativa 'ngsw-worker.js'
+      navigator.serviceWorker.register('ngsw-worker.js')
+        .then(() => console.log('🚀 Service Worker registrado manualmente con éxito'))
+        .catch(err => console.error('Error registrando SW:', err));
+    }
+  })
+  .catch(err => console.error(err));
